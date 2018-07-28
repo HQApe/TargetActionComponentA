93159f67071b29dd05cc887c22a3e81a2bb95f70
Ape <ape@ApedeMacBook-Pro.local>
2018-07-28 17:57:04 +0800

    Initial commit

---
+++ Example/TargetActionComponentA/HQViewController.m
@@ -0,0 +1,29 @@
+//
+//  HQViewController.m
+//  TargetActionComponentA
+//
+//  Created by HQApe on 07/28/2018.
+//  Copyright (c) 2018 HQApe. All rights reserved.
+//
+
+#import "HQViewController.h"
+
+@interface HQViewController ()
+
+@end
+
+@implementation HQViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
